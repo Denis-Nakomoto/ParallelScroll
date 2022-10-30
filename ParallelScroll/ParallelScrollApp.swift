210043c232aa730e7b5be0bb40c8276b554e57de
Denis Svetlakov <dps150482@gmail.com>
2022-10-30 13:41:00 +1000

    Initial Commit

---
+++ ParallelScroll/ParallelScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallelScrollApp.swift
+//  ParallelScroll
+//
+//  Created by Denis Svetlakov on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParallelScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
